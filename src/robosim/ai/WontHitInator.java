4049434dbe9265715fe22f6f826c8e31ccdd4ff0
Johannes <johannesm763@gmail.com>
2023-09-01 16:00:53 -0500

    first draft of not hitinator, just moves forwards and backwards

---
+++ src/robosim/ai/WontHitInator.java
@@ -0,0 +1,88 @@
+package robosim.ai;
+
+import robosim.core.Controller;
+import robosim.core.Direction;
+import robosim.core.Polar;
+import robosim.core.Simulator;
+import robosim.reinforcement.QTable;
+
+import java.util.Optional;
+
+public class WontHitInator implements Controller {
+
+    private QTable qTable;
+
+    public WontHitInator() {
+        int states = 100;
+        int actions = 2;
+        int startState = 0;
+        int targetVisits = 50;
+        int rateConstant = 5;
+        double discount = 0.9;
+
+        qTable = new QTable(states, actions, startState, targetVisits, rateConstant, discount);
+    }
+
+        private int getState(Simulator sim) {
+        double distanceToEdge = sim.findClosestEdge();
+        int edgeProximity;
+        if (distanceToEdge < 10) {
+            edgeProximity = 0;
+        } else if (distanceToEdge < 30) {
+            edgeProximity = 1;
+        } else {
+            edgeProximity = 2;
+        }
+
+        Optional<Polar> closestObstacle = sim.findClosestObstacle();
+        int obstacleProximity;
+        if (closestObstacle.isPresent()) {
+            double distance = closestObstacle.get().getR();
+            if (distance < 10) {
+                obstacleProximity = 0;
+            } else if (distance < 30) {
+                obstacleProximity = 1;
+            } else {
+                obstacleProximity = 2;
+            }
+        } else {
+            obstacleProximity = 3;
+        }
+
+
+        int motionState;
+        if (sim.getAngularVelocity() == 0 && sim.getTranslationalVelocity() > 0) {
+            motionState = 0;  // Moving forward
+        } else if (sim.getAngularVelocity() != 0) {
+            motionState = 1;  // Turning
+        } else {
+            motionState = 2;  // Stationary
+        }
+
+        int state = edgeProximity + obstacleProximity * 3 + motionState * 12;
+
+    return state;
+}
+    @Override
+    public void control(Simulator sim) {
+        int currentState = getState(sim);
+
+        double reward = 0;
+        if(sim.wasHit()) {
+            reward = -100;
+        }
+        else if(sim.getTranslationalVelocity() > 0) {
+            reward = 1;
+        }
+
+        int nextActionIndex = qTable.senseActLearn(currentState, reward);
+
+        Direction nextAction = Direction.values()[nextActionIndex];
+
+        if(nextAction == Direction.FWD || nextAction == Direction.REV) {
+            sim.translate(nextAction);
+        } else {
+            sim.turn(nextAction);
+        }
+    }
+}
