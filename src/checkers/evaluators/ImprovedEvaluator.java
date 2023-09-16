5ab6b7374154689bc96d10e4eeda5289e957b34b
Johannes253 <johannesm763@gmail.com>
2023-09-15 22:21:11 -0500

    added ImprovedEvaluator, evaluated position, value of piece and value of king

---
+++ src/checkers/evaluators/ImprovedEvaluator.java
@@ -0,0 +1,47 @@
+package checkers.evaluators;
+
+import checkers.core.Checkerboard;
+import checkers.core.PlayerColor;
+
+import java.util.function.ToIntFunction;
+
+public class ImprovedEvaluator implements ToIntFunction<Checkerboard> {
+
+    private static final int valuePiece = 10;
+    private static final int valueKing = 20;
+    private static final int valuePos = 2;
+
+    @Override
+    public int applyAsInt(Checkerboard board) {
+        PlayerColor currentPlayer = board.getCurrentPlayer();
+        PlayerColor enemy;
+
+        if(currentPlayer == PlayerColor.RED)
+            enemy = PlayerColor.BLACK;
+        else
+            enemy = PlayerColor.RED;
+
+        int currentPlayerValue = board.numPiecesOf(currentPlayer) * valuePiece +
+                board.numKingsOf(currentPlayer) * valueKing +
+                getValuePos(board, currentPlayer);
+
+        int enemyValue = board.numPiecesOf(enemy) * valuePiece +
+                board.numKingsOf(enemy) * valueKing +
+                getValuePos(board, enemy);
+
+        return currentPlayerValue - enemyValue;
+    }
+
+    private int getValuePos(Checkerboard board, PlayerColor currentPlayer) {
+        int posValue = 0;
+        for (int r = board.minRow(); r <= board.maxRow(); r++) {
+            for (int c = board.minCol(); c <= board.maxCol(); c++) {
+                if(board.colorAt(r, c, currentPlayer))
+                 posValue += (currentPlayer == PlayerColor.RED) ? (board.maxRow() - r) * valuePos : r * valuePos;
+            }
+
+            }
+        return posValue;
+    }
+
+}
